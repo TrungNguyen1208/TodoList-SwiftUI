89ec81da5fc16f5a79050ed11530bba1713755c8
Evans Nguyen <thanhtrungnguyen1208@gmail.com>
2022-12-14 22:22:12 +0700

    Initial Commit

---
+++ TodoList-SwiftUI/TodoList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_SwiftUIApp.swift
+//  TodoList-SwiftUI
+//
+//  Created by Trung Nguyen on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
